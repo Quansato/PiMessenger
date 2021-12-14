ca319a8e6b1c5a71c24d2abd4e3c6e85f1c2eccd
Quansato <ASUS@LAPTOP-UTNLUO6F>
2021-12-14 15:56:37 +0700

    toastr

--- PiChat/src/app/common/component/app-toolbar/app-toolbar.component.ts
+++ PiChat/src/app/common/component/app-toolbar/app-toolbar.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './app-toolbar.component.html',
-  styleUrls: ['./app-toolbar.component.scss']
+  styleUrls: ['./app-toolbar.component.scss'],
 })
 export class AppToolbarComponent implements OnInit {
-
-  constructor() { }
+  constructor(private toastr: ToastrService) {}
 
   ngOnInit(): void {
+    console.log("init")
+    this.toastr.success('Hello world!', 'Toastr fun!');
   }
-
 }
