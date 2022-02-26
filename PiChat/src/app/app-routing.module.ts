2f7bfa73f9d70b34ee412cdc8315ef3e78341d1f
Quansato <ASUS@LAPTOP-UTNLUO6F>
2022-02-26 22:02:16 +0700

    cap nhat moi

--- PiChat/src/app/app-routing.module.ts
+++ PiChat/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'chat', pathMatch: 'full' },
+  {
+    path: 'chat',
+    loadChildren: () =>
+      import('../app/component/conversation/conversation.module').then(
+        (m) => m.ConversationModule
+      ),
+      canActivate:[]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
